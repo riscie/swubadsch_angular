b7c4f760b3743f8a93205aa1e5eae3e9378e274e
Matthias Langhard <matthias@langaust.com>
2014-11-02 11:29:34 +0100

    Added$dsn usage

--- api/index.php
+++ api/index.php
@@ -2,6 +2,12 @@
 
 
 include('connection.php'); // includes the connection with Username / Password for the MySql DB
+//Usage:
+//Change the $dsn variable located at the top, here are some examples:
+//    SQLite: $dsn = 'sqlite://./path/to/database.sqlite';
+//    MySQL: $dsn = 'mysql://[user[:pass]@]host[:port]/db/;
+//    PostgreSQL: $dsn = 'pgsql://[user[:pass]@]host[:port]/db/;
+
 $clients = [];
 
 
